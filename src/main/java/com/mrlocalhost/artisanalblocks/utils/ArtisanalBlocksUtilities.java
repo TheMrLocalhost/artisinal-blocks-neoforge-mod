eb9e5ba321195370da9e0a931c16f37ccd31fd76
TheMrLocalhost <the@mrlocalhost.com>
2025-07-21 23:57:57 -0500

    swapped in HalfTransparentBlock blocks to exclude list

--- src/main/java/com/mrlocalhost/artisanalblocks/utils/ArtisanalBlocksUtilities.java
+++ src/main/java/com/mrlocalhost/artisanalblocks/utils/ArtisanalBlocksUtilities.java
@@ -4,6 +4,7 @@ import com.mrlocalhost.artisanalblocks.block.ModBlocks;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.HalfTransparentBlock;
 
 
 public class ArtisanalBlocksUtilities {
@@ -17,6 +18,7 @@ public class ArtisanalBlocksUtilities {
             return (item.getItem() instanceof BlockItem blockItem)
                 && !(blockItem.getBlock() instanceof BaseEntityBlock)
                 && (blockItem.getBlock().defaultBlockState().isCollisionShapeFullBlock(null, null))
+                && !(blockItem.getBlock() instanceof HalfTransparentBlock)
             ;
         } catch (Exception ignored) {
             return false;
