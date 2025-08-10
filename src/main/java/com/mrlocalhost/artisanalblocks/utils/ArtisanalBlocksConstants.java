06516fbdb1c29db57d3473a8194c720540d64c96
TheMrLocalhost <the@mrlocalhost.com>
2025-08-09 22:27:11 -0500

    Added Mod icon, removed debugging restone particles, and fixed eyedropper copy issue

--- src/main/java/com/mrlocalhost/artisanalblocks/utils/ArtisanalBlocksConstants.java
+++ src/main/java/com/mrlocalhost/artisanalblocks/utils/ArtisanalBlocksConstants.java
@@ -3,7 +3,6 @@ package com.mrlocalhost.artisanalblocks.utils;
 import com.mrlocalhost.artisanalblocks.block.custom.ArtisanalBlock;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.phys.Vec3;
 
 import java.util.List;
 import java.util.Map;
@@ -21,17 +20,6 @@ public class ArtisanalBlocksConstants {
         Direction.SOUTH,
         Direction.WEST,
         Direction.EAST
-    );
-
-    public static final List<Vec3> BLOCK_CORNERS = List.of(
-        new Vec3(0, 0, 0),
-        new Vec3(0, 0, 1),
-        new Vec3(0, 1, 0),
-        new Vec3(0, 1, 1),
-        new Vec3(1, 0, 0),
-        new Vec3(1, 0, 1),
-        new Vec3(1, 1, 0),
-        new Vec3(1, 1, 1)
     );
 
     public static final Map<ArtisanalBlockConfigs.BLOCK_OPTIONS, BooleanProperty> BLOCK_CONFIG_PROPERTY_MAP = Map.ofEntries(
